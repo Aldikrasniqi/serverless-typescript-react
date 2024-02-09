87a388aced64998bd14de62ad45a90207d289de9
Aldi Krasniqi <58590311+Aldikrasniqi@users.noreply.github.com>
2024-02-09 10:29:38 +0100

    Update submit.ts cors

--- netlify/functions/submit.ts
+++ netlify/functions/submit.ts
@@ -5,6 +5,12 @@ export const handler: Handler = async (event) => {
   console.log(`Hello ${name}, your favorite color is ${favoriteColor}`);
   return {
     statusCode: 200,
+    headers: {
+      /* Required for CORS support to work */
+      'Access-Control-Allow-Origin': '*',
+      /* Required for cookies, authorization headers with HTTPS */
+      'Access-Control-Allow-Credentials': true
+    },
     body: JSON.stringify({ 
       name,
       favoriteColor,
